63731f6d72c857b073e80a697404bc7ce9556e44
MilanVeul <milan@veul.net>
2026-04-26 20:51:43 +0200

    Forward euler implementation

---
+++ forward_euler.py
@@ -0,0 +1,45 @@
+import numpy as np
+from common import import_data, TOTAL_INFECTIONS_COL, TOTAL_RECOVERIES_COL, INFECTED_COL
+
+
+
+def forward_euler(S_init, I_init, R_init, beta, gamma, dt, duration):
+    """
+    Solves the SIR model using Forward Euler method.
+    """
+    N = S_init + I_init + R_init
+    
+    num_steps = int(duration / dt)
+    S = np.zeros(num_steps)
+    I = np.zeros(num_steps)
+    R = np.zeros(num_steps)
+    
+    # Set initial values
+    S[0], I[0], R_init = S_init, I_init, R_init
+    
+    for t in range(num_steps - 1):
+        S_rate = -(beta * S[t] * I[t]) / N
+        I_rate = (beta * S[t] * I[t]) / N - (gamma * I[t])
+        R_rate = gamma * I[t]
+        
+        S[t+1] = S[t] + dt*S_rate
+        I[t+1] = I[t] + dt*I_rate
+        R[t+1] = R[t] + dt*R_rate
+        
+    return S, I, R
+
+# As approximated in assignment 3
+beta = 0.8826642165086095
+gamma = 0.2501883071115894
+
+df = import_data()
+N = max(df[TOTAL_INFECTIONS_COL])
+R_init = df[TOTAL_RECOVERIES_COL][0]
+I_init = df[TOTAL_INFECTIONS_COL][0]
+S_init = N - I_init
+
+S, I, R = forward_euler(S_init, I_init, R_init, beta, gamma, 0.01, 1000)
+
+# Print all 
+total = (S + I + R).astype(int)
+print(np.unique(total))
