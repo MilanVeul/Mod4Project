719fb3873456fd1039193824219976b4b23b625f
MilanVeul <milan@veul.net>
2026-05-11 09:30:55 +0200

    Only plot simulation

--- forward_euler.py
+++ forward_euler.py
@@ -32,7 +32,7 @@ def forward_euler(S_init, I_init, R_init, beta, gamma, dt, duration):
 # As approximated in assignment 3
 beta = 0.4195460684174166
 gamma = 0.2647898334098834
-dt = 0.01
+dt = 0.1
 
 df = import_data()
 N_init = 17_500_000
@@ -40,7 +40,7 @@ R_init = df[TOTAL_RECOVERIES_COL][0]
 I_init = df[INFECTED_COL][0]
 S_init = N_init - I_init
 
-S, I, R = forward_euler(S_init, I_init, R_init, beta, gamma, dt, 20)
+S, I, R = forward_euler(S_init, I_init, R_init, beta, gamma, dt, 160)
 N = S + I + R
 
 # Generate Date Axis
@@ -49,25 +49,26 @@ start_date = pd.to_datetime(df[DATE_COL].iloc[0])
 simulation_dates = start_date + pd.to_timedelta(np.arange(len(S)) * dt, unit='D')
 
 # Print all
-plt.figure(figsize=(10, 6))
-plt.plot(simulation_dates, N, marker="", linestyle="-")
-plt.title("Total Population Size of Simulation")
-plt.xlabel("Time")
-plt.ylabel("Individuals")
-plt.ylim(N[0] * 0.99, N[0] * 1.01)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.grid()
-plt.savefig("diagrams/population_size.png")
+# plt.figure(figsize=(10, 6))
+# plt.plot(simulation_dates, N, marker="", linestyle="-")
+# plt.title("Total Population Size of Simulation")
+# plt.xlabel("Time")
+# plt.ylabel("Individuals")
+# plt.ylim(N[0] * 0.99, N[0] * 1.01)
+# plt.xticks(rotation=45)
+# plt.tight_layout()
+# plt.grid()
+# plt.savefig("diagrams/population_size.png")
 
 
 plt.figure(figsize=(10, 6))
 plt.plot(simulation_dates, I, marker="", linestyle="-")
 plt.plot(simulation_dates, R, marker="", linestyle="-")
-plt.plot(df["Date"], df[INFECTED_COL], marker="o", linestyle="-")
-plt.plot(df["Date"], df[TOTAL_RECOVERIES_COL], marker="o", linestyle="-")
+# plt.plot(df["Date"], df[INFECTED_COL], marker="o", linestyle="-")
+# plt.plot(df["Date"], df[TOTAL_RECOVERIES_COL], marker="o", linestyle="-")
 plt.plot()
-plt.legend(["Simulated Infected", "Simulated Recovered", "Actual Infected", "Actual Recovered"])
+# plt.legend(["Simulated Infected", "Simulated Recovered", "Actual Infected", "Actual Recovered"])
+plt.legend(["Simulated Infected", "Simulated Recovered"])
 plt.title("Susceptable, Recovered and Infected Individuals over Time")
 plt.xlabel("Time")
 plt.ylabel("Individuals")
